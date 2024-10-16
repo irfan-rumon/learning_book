1b937213b09e0a69140c83e3e34827468602ec5a
irfan-rumon <irfannisho8571@gmail.com>
2024-10-16 15:31:53 +0600

    Add lang file

---
+++ lang/en/local_certificate.php
@@ -0,0 +1,20 @@
+<?php
+
+// English language strings for local_certificate_manager plugin
+
+$string['pluginname'] = 'Learning Book'; // Name of your plugin as it will appear in Moodle
+$string['receiveddate'] = 'Received Date';
+$string['expireddate'] = 'Expired Date';
+$string['fileupload'] = 'File Upload';
+$string['coursename'] = 'Course Name';
+$string['internal'] = 'Internal';
+$string['external'] = 'External';
+$string['uploadimage'] = 'Upload Image';
+$string['uploadcertificate'] = 'Upload Certificate';
+$string['username'] = 'User name';
+$string['uploadfile'] = 'Upload File';
+$string['viewpdf'] = 'Upload Image';
+$string['username'] = 'User  Name';
+$string['courseid'] = 'Course ID';
+$string['username'] = 'User Name';
+$string['error_alreadyexists'] = 'Already Exists! Add a new one.';
